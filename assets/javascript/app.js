1b2b827cd3f87cbeb9a7e3d5097545d6e06a0667
calebkloeckner <calebkloeckner@gmail.com>
2017-12-21 21:24:46 -0700

    test 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,6 @@ const imdataSKey = "MyMMQ2xFkP5oSwSAK6Z8Dkd4Nz5z6qse";
 var imdataProduct = "";
 $(document).ready(function() {
   
-FB.getLoginStatus(function(response) {
-  statusChangeCallback(response);
-});
     var imdataProduct = "product_identifier=014100044208";
     var imURL = "https://www.iamdata.co/v1/products?"+imdataProduct+"&page=1&per_page=10&full_resp=false&client_id=" + imdataKey + "&client_secret=" + imdataSKey;
     $.ajax({
