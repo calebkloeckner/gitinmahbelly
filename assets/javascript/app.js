adacd0c99b4c53eec2973954a972e56b713eb5e5
madkcat <30823405+madkcat@users.noreply.github.com>
2017-12-21 20:15:45 -0700

    Facebook logins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,10 @@ const imdataKey = "HEFEvhlA";
 const imdataSKey = "MyMMQ2xFkP5oSwSAK6Z8Dkd4Nz5z6qse";
 var imdataProduct = "";
 $(document).ready(function() {
+  
+FB.getLoginStatus(function(response) {
+  statusChangeCallback(response);
+});
     var imdataProduct = "product_identifier=014100044208";
     var imURL = "https://www.iamdata.co/v1/products?"+imdataProduct+"&page=1&per_page=10&full_resp=false&client_id=" + imdataKey + "&client_secret=" + imdataSKey;
     $.ajax({
