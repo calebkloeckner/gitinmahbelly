26988e13d3d4a312907aa5670cadf3d699e1da33
madkcat <30823405+madkcat@users.noreply.github.com>
2018-01-04 20:49:32 -0700

    update api keys, functional edamam
    a

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,24 @@
 const gifkey = "JFlbvY7o0WgDcLH8D9vc7jlFL5hMW2Dl";
-const imdataKey = "HEFEvhlA";
-const imdataSKey = "MyMMQ2xFkP5oSwSAK6Z8Dkd4Nz5z6qse";
+const imdataKey = "c6TNwjpz";
+const imdataSKey = "J8adbvz3Bs5KLJ2nTeFRXSAwKNUanpar";
 var imdataProduct = "";
+
+const edamAppID = "c1724022";
+const edamAppKey = "cdded1f6d7a29716aec7adcec57b419e";
 
 
 
 $(document).ready(function() {
-  
-  
+    var edamIng = 'Margarita';  //Spaces need to be translated to %20. required  
+    var edamURL = 'https://api.edamam.com/search?q='+edamIng+'&app_id='+edamAppID+'&app_key='+edamAppKey;
+    $.ajax({
+      url: edamURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response)
+    });
+
+    
     // var imdataProduct = "product_identifier=014100044208";
     // var imURL = "https://www.iamdata.co/v1/products?"+imdataProduct+"&page=1&per_page=10&full_resp=false&client_id=" + imdataKey + "&client_secret=" + imdataSKey;
     // $.ajax({
@@ -16,5 +27,6 @@ $(document).ready(function() {
     // }).done(function(response) {
     //   console.log(response)
     // });
-    
+
+
 });
