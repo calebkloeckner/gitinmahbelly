fb0504099dd9f8e3b034e4585f9fccfe702e8a1c
calebkloeckner <calebkloeckner@gmail.com>
2018-01-06 12:19:40 -0700

    images showing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
-const gifkey = "JFlbvY7o0WgDcLH8D9vc7jlFL5hMW2Dl";
-const imdataKey = "HEFEvhlA";
-const imdataSKey = "MyMMQ2xFkP5oSwSAK6Z8Dkd4Nz5z6qse";
-var imdataProduct = "";
-$(document).ready(function() {
+// const gifkey = "JFlbvY7o0WgDcLH8D9vc7jlFL5hMW2Dl";
+// const imdataKey = "HEFEvhlA";
+// const imdataSKey = "MyMMQ2xFkP5oSwSAK6Z8Dkd4Nz5z6qse";
+// var imdataProduct = "";
+// $(document).ready(function() {
   
-    var imdataProduct = "product_identifier=014100044208";
-    var imURL = "https://www.iamdata.co/v1/products?"+imdataProduct+"&page=1&per_page=10&full_resp=false&client_id=" + imdataKey + "&client_secret=" + imdataSKey;
-    $.ajax({
-      url: imURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log(response)
-    });
+//     var imdataProduct = "product_identifier=014100044208";
+//     var imURL = "https://www.iamdata.co/v1/products?"+imdataProduct+"&page=1&per_page=10&full_resp=false&client_id=" + imdataKey + "&client_secret=" + imdataSKey;
+//     $.ajax({
+//       url: imURL,
+//       method: 'GET'
+//     }).done(function(response) {
+//       console.log(response)
+//     });
     
-});
+// });
