72c3947eba2e013195082eecf4794f3a7ae4a3ea
madkcat <30823405+madkcat@users.noreply.github.com>
2018-01-12 04:16:05 -0700

    variable redirect to restrict api calls from about.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,28 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+const gifkey = "JFlbvY7o0WgDcLH8D9vc7jlFL5hMW2Dl";
+const imdataKey = "c6TNwjpz";
+const imdataSKey = "J8adbvz3Bs5KLJ2nTeFRXSAwKNUanpar";
+var imdataProduct = "";
+var runI = 0;
+
+// const edamAppID = "c1724022";
+// const edamAppKey = "cdded1f6d7a29716aec7adcec57b419e";
+//local test key
+const edamAppID = "926110ce";
+const edamAppKey = "c284b239fa0287a8009b4ce59afed42c";
+
+var edamIng = [];  //Spaces need to be translated to %20. required 
+var edamIngRandom = ['beef', 'pork', 'chicken', 'ramen', 'apple%20cherry', 'milk%20corn', 'curry']
+edamIng.push(edamIngRandom[getRandomInt(edamIngRandom.length)]);
+var edamURL = 'https://api.edamam.com/search?q='+edamIng+'&app_id='+edamAppID+'&app_key='+edamAppKey;
+
+var testResponse = {};
+
+
+
 $(document).ready(function(){
     
     $('.content').mouseenter(function() {
