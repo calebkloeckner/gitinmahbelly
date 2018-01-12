be5975e92245078bde1dd22715ab73b30e3077af
madkcat <30823405+madkcat@users.noreply.github.com>
2018-01-12 04:10:07 -0700

    cleaned redundant console.log and obsolete code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -57,6 +57,10 @@ $(document).ready(function () {
     
     topRecipes();
 
+
+    //Search Bar Functionality 
+    // creates ingredient buttons
+    // ---> Ends with call to $('.search-button').click();
     $.ajax({
       url: edamURL,
       method: 'GET'
@@ -88,7 +92,7 @@ $(document).ready(function () {
             }
         });
 
-
+        // removes buttons created by the search bar
         $(document).on('click', ".ingredient-button", function () {
           let removeAct = $(this).attr("data-button");
           let removeActId = $(this).attr("id");
@@ -97,128 +101,88 @@ $(document).ready(function () {
           $(this).remove(); 
           edamIng.splice(indexInArrayToRemove, 1);
         });
-          
+        
+        // runs API call
         $('.search-button').on('click', function () {
           for (let i = 0; i < edamIng.length; i++) {
             edamIng = edamIng.toString().replace(",", "%20");
-            console.log(edamIng);
-            console.log("---");
           };
           var edamURL = 'https://api.edamam.com/search?q='+edamIng+'&app_id='+edamAppID+'&app_key='+edamAppKey;
             console.log("search executing");
-            console.log(edamIng);
-            console.log(edamURL);
             $.ajax({
-                url: edamURL,
-                method: 'GET'
+              url: edamURL,
+              method: 'GET'
             }).done(function (response) {
-                console.log(response);
                 if (response.count >=8) {
                   for (let i = 0; i < 8; i++) 
                     recipes.push({
-                        title: response.hits[i].recipe.label,
-                        ingredients: response.hits[i].recipe.ingredientLines,
-                        image: response.hits[i].recipe.image,
-                        link: response.hits[i].recipe.url
-                    })}
+                      title: response.hits[i].recipe.label,
+                      ingredients: response.hits[i].recipe.ingredientLines,
+                      image: response.hits[i].recipe.image,
+                      link: response.hits[i].recipe.url
+                    })
+                  }
                 else {
-                    for (let i = 0; i < response.count; i++) {
-                        recipes.push({
-                            title: response.hits[i].recipe.label,
-                            ingredients: response.hits[i].recipe.ingredientLines,
-                            image: response.hits[i].recipe.image,
-                            link: response.hits[i].recipe.url
-                        })
-                };
-                    // console.log(response.hits[i].recipe.image);
+                  for (let i = 0; i < response.count; i++) {
+                    recipes.push({
+                      title: response.hits[i].recipe.label,
+                      ingredients: response.hits[i].recipe.ingredientLines,
+                      image: response.hits[i].recipe.image,
+                      link: response.hits[i].recipe.url
+                    })
+                  };
                 }
-                // function topRecipes() {
-                //     console.log(recipes);
-                //     $("#top-recipes").empty();
-                //     for (var i = 0; i < recipes.length; i++) {
-                //         console.log(recipes[i]);
-                //         $("#top-recipes").prepend(`<button class="food" data-button='${JSON.stringify(recipes[i])}' data-id="${i}"><img src="${recipes[i].image}"></button>`);
-                        
-                //         // Test code to give buttons a dynamic id name ~ obsolete?
-                //         // var custAttr = "id-"+i;
-                //         // console.log(custAttr);
-                //         // $("#top-recipes").prepend(`<button class='food' data-button='${JSON.stringify(recipes[i])}' data-id='${i}'  ${custAttr}='${i}'><img src='${recipes[i].image}'></button>`);
-                //         // console.log(recipes[i]);
-                
-                //         // $("#top-recipes").attr("open", recipes[i]);
-                //         // console.log(recipes);
-                //     }
-                // }
-                       
-                console.log(edamIng);
-                console.log(response);
                 topRecipes();
             });
-            
         });
 
       testResponse = response;
       var ingLoop = response.hits[0].recipe.ingredients;
-    //   console.log(response)
-    //   console.log(response.hits[0].recipe.image)
-    //   console.log(response.hits[0].recipe.label)
-    //   for (i = 0; i < ingLoop.length; i++) {
-    //   console.log(ingLoop[i].text)}
-    //   console.log(response.hits[0].recipe.source)
-    //   console.log(response.hits[0].recipe.url)
-    //   console.log(response.hits[0].recipe.yield)
-    //   console.log(testResponse)
     });
+
     // modal display
     $('.modal').modal();
-    // on click function to open the modal
-    $(document).on('click', ".foodish", function () {
+      // on click function to open the modal
+      $(document).on('click', ".foodish", function () {
         console.log(this)
         let clicky = JSON.parse($(this).attr("alt"));
         let clickedId = $(this).attr("data-id");
         console.log(clicky);
         $('#modal1').modal('open');
-        // displays the object insie the modal
-        let recipeTitle = recipes[clickedId].title;
-        let recipeBody = recipes[clickedId].ingredients;
-        let recipeImage = recipes[clickedId].image;
-        let recipeUrl = recipes[clickedId].link;
-       
-        console.log(recipeTitle);
-        console.log(clicky.ingredients);
-        // for loop to add each ingredient to its own line with a <p> tag
-        let i;
-        for (i = 0; i < clicky.ingredients.length; i++) {
+          // displays the object insie the modal
+          let recipeTitle = recipes[clickedId].title;
+          let recipeBody = recipes[clickedId].ingredients;
+          let recipeImage = recipes[clickedId].image;
+          let recipeUrl = recipes[clickedId].link;
+          console.log(recipeTitle);
+          console.log(clicky.ingredients);
+          // for loop to add each ingredient to its own line with a <p> tag
+          let i;
+          for (i = 0; i < clicky.ingredients.length; i++) {
             let item = $('<p>').html(clicky.ingredients[i]);
             $("#recipe-body").append(item);
-        }
-        $("#recipe-name").html(recipeTitle).append("<img src='#' id='recipe-image'>");
-        
-        // <div class="content-overlay">
-
-        // $("#recipe-body").attr(recipeBody); this was the old way. Don't think its needed
-        $("#recipe-image").attr("src", recipeImage);
-        $("#recipe-url").attr("href", recipeUrl);
-        console.log(recipeUrl);
-        console.log("boogers");
-    });
+          }
+          $("#recipe-name").html(recipeTitle).append("<img src='#' id='recipe-image'>");
+          $("#recipe-image").attr("src", recipeImage);
+          $("#recipe-url").attr("href", recipeUrl);
+          console.log(recipeUrl);
+          console.log("boogers");
+      });
       
-    $.ajax({
+      $.ajax({
         url: edamURL,
         method: 'GET'
-    }).done(function (response) {
-        console.log(response);
-        for (let i = 0; i < 8; i++) {
+      }).done(function (response) {
+          console.log(response);
+          for (let i = 0; i < 8; i++) {
             recipes.push({
-                title: response.hits[i].recipe.label,
-                ingredients: response.hits[i].recipe.ingredientLines,
-                image: response.hits[i].recipe.image,
-                link: response.hits[i].recipe.url
+              title: response.hits[i].recipe.label,
+              ingredients: response.hits[i].recipe.ingredientLines,
+              image: response.hits[i].recipe.image,
+              link: response.hits[i].recipe.url
             });
-            console.log(response.hits[i].recipe.image);
-
-
-        }
-        topRecipes();
-    });
+          console.log(response.hits[i].recipe.image);
+          }
+          topRecipes();
+      });
 });
